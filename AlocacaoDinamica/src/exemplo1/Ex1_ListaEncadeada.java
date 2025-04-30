69f09f7d78e074060214e179dc1fa230bc00cf0a
Guilhermo <guilhermo.payossin@gmail.com>
2025-04-30 20:52:35 -0300

    Add files via upload

---
+++ AlocacaoDinamica/src/exemplo1/Ex1_ListaEncadeada.java
@@ -0,0 +1,16 @@
+package exemplo1;
+
+public class Ex1_ListaEncadeada {
+    public static void main(String[] args) {
+        Elemento p1 = new Elemento();
+        p1.dado = 1;
+        System.out.println("p1: " + p1 + "\tdado: " + p1.dado);
+        Elemento p2 = new Elemento();
+        p2.dado = 2;
+        System.out.println("p2: " + p2 + "\tdado: " + p2.dado);
+        Elemento p3 = new Elemento();
+        p3.dado = 3;
+        System.out.println("p3: " + p3 + "\tdado: " + p3.dado);
+    }
+
+}
